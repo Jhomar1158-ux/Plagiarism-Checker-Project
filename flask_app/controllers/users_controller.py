ee263e38993c91e2c8565af8b063e1a346579fba
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-05-29 12:49:47 -0500

    sessions listas

--- flask_app/controllers/users_controller.py
+++ flask_app/controllers/users_controller.py
@@ -1,7 +1,31 @@
-from flask import render_template, redirect, request, get_flashed_messages
+
+from flask import render_template, redirect, request, session
 from flask_app import app
 
 @app.route("/")
 def index():
-    return render_template("dashboard.html")
+    if "texto1" in session:
+        texto1 = session["texto1"]
+    else:
+        texto1=''
+
+    if "texto2" in session:
+        texto2 = session["texto2"]
+    else:
+        texto2=''
+
+    
+    return render_template("dashboard.html", texto1=texto1, texto2=texto2)
+
+
+@app.route("/plagiarism-checker", methods=["POST"])
+def check_plagiarism():
+    print(request.form)
+    texto1 = request.form["text1"]
+    texto2= request.form["text2"]
+    session["texto1"] = texto1
+    session["texto2"]= texto2
+
+    return redirect("/")
+
 
--- flask_app/script.py
+++ flask_app/script.py
@@ -1,6 +1,12 @@
 import os
-listdir=["CSS","JS","IMG"]
-for i in listdir:
-    os.makedirs(f"static/{i}")
+# listdir=["CSS","JS","IMG"]
+# for i in listdir:
+#     os.makedirs(f"static/{i}")
 
+
+student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+student_notes = [open(_file, encoding='utf-8').read() for _file in student_files]
+
+print(student_files)
+print(student_notes)
 
